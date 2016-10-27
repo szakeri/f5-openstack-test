db7bdf7114c0e9ac0a134c2ce0bbc234cca0d4d1
Za Wilgustus <za@f5.com>
2016-10-27 15:03:49 +0000

    Add logging

--- f5_os_test/testrunner_containers/docker_manager.py
+++ f5_os_test/testrunner_containers/docker_manager.py
@@ -46,6 +46,9 @@ def build_container(test_type, project):
     build_string = "docker build -t {} -f {} {}".format(registry_fullname,
                                                         project_dockerfile,
                                                         '.')
+    logger.debug(registry_fullname)
+    logger.debug(project_dockerfile)
+    logger.debug(build_string)
     subprocess.check_call(build_string.split())
     pubstring = "docker push {}".format(registry_fullname)
     subprocess.check_call(pubstring.split())
