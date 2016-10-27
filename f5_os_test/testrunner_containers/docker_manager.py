2a909223cd9608ca353c2239930692fe84930cc6
Za Wilgustus <za@f5.com>
2016-10-27 16:01:32 +0000

    wip

--- f5_os_test/testrunner_containers/docker_manager.py
+++ f5_os_test/testrunner_containers/docker_manager.py
@@ -55,7 +55,7 @@ def build_container(test_type, project):
     logger.debug(project_dockerfile)
     logger.debug(build_string)
     logger.debug('curdir: {}'.format(os.path.abspath(os.curdir)))
-    echores = subprocess.check_output('echo $PWD'.split(),
+    echores = subprocess.check_output('echo -n $PWD'.split(),
                           cwd=CURRENTDIR)
     logger.debug(echores)
     #subprocess.check_call(build_string.split(), cwd=CURRENTDIR)
